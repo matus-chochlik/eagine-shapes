a7a3df47b6c99a4d5a9ede47cc0830bc0426a2b0
Matus Chochlik <chochlik@gmail.com>
2021-10-08 08:05:11 +0200

    Added the add_occlusion executable

---
+++ source/app/add_occlusion/main.cpp
@@ -0,0 +1,43 @@
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+#include <eagine/main.hpp>
+#include <eagine/shapes/from_json.hpp>
+#include <eagine/shapes/occluded.hpp>
+#include <eagine/shapes/to_json.hpp>
+#include <eagine/valid_if/positive.hpp>
+#include <iostream>
+
+namespace eagine {
+
+static auto get_base_generator(main_ctx& ctx)
+  -> std::unique_ptr<shapes::generator> {
+    // TODO: parse input file path
+    return shapes::from_json_stream(std::cin, ctx);
+}
+
+auto main(main_ctx& ctx) -> int {
+    using namespace eagine;
+
+    if(auto bgen{get_base_generator(ctx)}) {
+        valid_if_positive<span_size_t> samples{256};
+        if(const auto arg{ctx.args().find("--shape-occlusion-samples")}) {
+            arg.next().parse(samples, ctx.log().error_stream());
+        }
+        if(auto gen{
+             shapes::occlude(std::move(bgen), extract_or(samples, 256), ctx)}) {
+            shapes::to_json_options opts;
+            if(parse_from(ctx, extract(gen), opts)) {
+                opts.attrib_variants[shapes::vertex_attrib_kind::occlusion][0];
+                shapes::to_json(std::cout, extract(gen), opts) << std::endl;
+            }
+        }
+    }
+
+    return 0;
+}
+
+} // namespace eagine
