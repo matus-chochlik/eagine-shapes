0284809b2aa4d03e4ae37a1bd0b19d925331f980
Matus Chochlik <chochlik@gmail.com>
2022-11-09 08:56:28 +0100

    Changed how default shape bounding sphere radius is calculated

--- source/modules/eagine/generator_impl.cpp
+++ source/modules/eagine/generator_impl.cpp
@@ -21,6 +21,8 @@ import <limits>;
 import <optional>;
 import <vector>;
 
+import <iostream>;
+
 namespace eagine::shapes {
 //------------------------------------------------------------------------------
 // generator
@@ -99,10 +101,9 @@ auto generator_base::bounding_sphere() -> math::sphere<float, true> {
     float radius{0.F};
     for(const auto c : integer_range(m)) {
         const integer k{c};
-        const auto q = (max[k] - min[k]) * 0.5F;
-        radius += q * q;
+        radius = math::maximum(radius, (max[k] - min[k]) * 0.25F);
     }
-    radius = std::sqrt(radius);
+    radius = std::sqrt(3 * radius * radius);
 
     return {center, radius};
 }
