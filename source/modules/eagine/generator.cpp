b9409186d3eb92ab4b70028d425e321edfecd559
Matus Chochlik <chochlik@gmail.com>
2023-03-05 10:02:54 +0100

    Merged some partitions

--- source/modules/eagine/generator.cpp
+++ source/modules/eagine/generator.cpp
@@ -16,7 +16,10 @@ import eagine.core.memory;
 import eagine.core.container;
 import eagine.core.reflection;
 import eagine.core.utility;
+import eagine.core.runtime;
 import eagine.core.math;
+import eagine.core.valid_if;
+import eagine.core.main_ctx;
 export import :vertex_attributes;
 export import :drawing;
 import std;
@@ -672,6 +675,106 @@ inline void generator_base::random_surface_values(
   [[maybe_unused]] const random_attribute_values& values) {
     assert(are_consistent(values));
 }
+//------------------------------------------------------------------------------
+// screen
+//------------------------------------------------------------------------------
+/// @brief Constructs instances of unit_screen_gen.
+/// @ingroup shapes
+/// @see from_value_tree
+/// @see unit_cube
+/// @see unit_round_cube
+/// @see unit_sphere
+/// @see unit_icosahedron
+/// @see unit_torus
+/// @see unit_twisted_torus
+export [[nodiscard]] auto unit_screen(const vertex_attrib_kinds attr_kinds)
+  -> std::unique_ptr<generator>;
+
+/// @brief Tries to construct instances of unit_screen_gen from an URL.
+/// @ingroup shapes
+export [[nodiscard]] auto unit_screen_from(
+  const vertex_attrib_kinds,
+  const url&,
+  main_ctx&) -> std::unique_ptr<generator>;
+//------------------------------------------------------------------------------
+// plane
+//------------------------------------------------------------------------------
+/// @brief Constructs instances of unit_plane_gen.
+/// @ingroup shapes
+/// @see unit_plane_from
+/// @see from_value_tree
+/// @see unit_screen
+/// @see unit_cube
+/// @see unit_round_cube
+/// @see unit_sphere
+/// @see unit_icosahedron
+/// @see unit_torus
+/// @see unit_twisted_torus
+export [[nodiscard]] auto unit_plane(
+  const vertex_attrib_kinds attr_kinds,
+  const valid_if_positive<int>& width,
+  const valid_if_positive<int>& height) -> std::unique_ptr<generator>;
+
+export [[nodiscard]] auto unit_plane(const vertex_attrib_kinds attr_kinds)
+  -> std::unique_ptr<generator> {
+    return unit_plane(attr_kinds, 4, 4);
+}
+
+/// @brief Tries to construct instances of unit_plane_gen from an URL.
+/// @ingroup shapes
+export [[nodiscard]] auto unit_plane_from(
+  const vertex_attrib_kinds,
+  const url&,
+  main_ctx&) -> std::unique_ptr<generator>;
+//------------------------------------------------------------------------------
+// cube
+//------------------------------------------------------------------------------
+/// @brief Constructs instances of unit_cube_gen.
+/// @ingroup shapes
+/// @see unit_cube_from
+/// @see from_value_tree
+/// @see unit_round_cube
+/// @see unit_icosahedron
+/// @see unit_sphere
+/// @see unit_torus
+/// @see unit_twisted_torus
+/// @see unit_screen
+export [[nodiscard]] auto unit_cube(const vertex_attrib_kinds attr_kinds)
+  -> std::unique_ptr<generator>;
+
+/// @brief Tries to construct instances of unit_cube_gen from an URL.
+/// @ingroup shapes
+export [[nodiscard]] auto unit_cube_from(
+  const vertex_attrib_kinds,
+  const url&,
+  main_ctx&) -> std::unique_ptr<generator>;
+//------------------------------------------------------------------------------
+// round_cube
+//------------------------------------------------------------------------------
+/// @brief Constructs instances of unit_round_cube_gen.
+/// @ingroup shapes
+/// @see unit_round_cube_from
+/// @see from_value_tree
+/// @see unit_cube
+/// @see unit_icosahedron
+/// @see unit_sphere
+/// @see unit_torus
+/// @see unit_twisted_torus
+/// @see unit_screen
+export [[nodiscard]] auto unit_round_cube(
+  const vertex_attrib_kinds attr_kinds,
+  const valid_if_positive<int> divisions) -> std::unique_ptr<generator>;
+//------------------------------------------------------------------------------
+export [[nodiscard]] auto unit_round_cube(const vertex_attrib_kinds attr_kinds) {
+    return unit_round_cube(attr_kinds, 8);
+}
+
+/// @brief Tries to construct instances of unit_round_cube_gen from an URL.
+/// @ingroup shapes
+export [[nodiscard]] auto unit_round_cube_from(
+  const vertex_attrib_kinds,
+  const url&,
+  main_ctx&) -> std::unique_ptr<generator>;
 //------------------------------------------------------------------------------
 } // namespace shapes
 }// namespace eagine
--- source/modules/eagine/round_cube.cpp
+++ source/modules/eagine/round_cube.cpp
@@ -15,30 +15,5 @@ import std;
 
 namespace eagine::shapes {
 //------------------------------------------------------------------------------
-/// @brief Constructs instances of unit_round_cube_gen.
-/// @ingroup shapes
-/// @see unit_round_cube_from
-/// @see from_value_tree
-/// @see unit_cube
-/// @see unit_icosahedron
-/// @see unit_sphere
-/// @see unit_torus
-/// @see unit_twisted_torus
-/// @see unit_screen
-export [[nodiscard]] auto unit_round_cube(
-  const vertex_attrib_kinds attr_kinds,
-  const valid_if_positive<int> divisions) -> std::unique_ptr<generator>;
-//------------------------------------------------------------------------------
-export [[nodiscard]] auto unit_round_cube(const vertex_attrib_kinds attr_kinds) {
-    return unit_round_cube(attr_kinds, 8);
-}
-
-/// @brief Tries to construct instances of unit_round_cube_gen from an URL.
-/// @ingroup shapes
-export [[nodiscard]] auto unit_round_cube_from(
-  const vertex_attrib_kinds,
-  const url&,
-  main_ctx&) -> std::unique_ptr<generator>;
-//------------------------------------------------------------------------------
 } // namespace eagine::shapes
 
