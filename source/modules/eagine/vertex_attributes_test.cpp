73dfda030a53acfb99043f2486af3c49833f4e60
Matus Chochlik <chochlik@gmail.com>
2024-03-16 11:12:17 +0100

    Added unit test

--- source/modules/eagine/vertex_attributes_test.cpp
+++ source/modules/eagine/vertex_attributes_test.cpp
@@ -32,9 +32,27 @@ void vertex_attrib_kinds_all(auto& s) {
     test.check(all_attrs == test_attrs, "all set");
 }
 //------------------------------------------------------------------------------
+void vertex_attrib_kinds_index(auto& s) {
+    eagitest::case_ test{s, 2, "index"};
+    eagitest::track trck{test, 0, 1};
+
+    for(const auto& info :
+        eagine::enumerators<eagine::shapes::vertex_attrib_kind>()) {
+
+        test.check(
+          info.enumerator ==
+            eagine::shapes::vertex_attrib_by_index(
+              eagine::shapes::vertex_attrib_index(info.enumerator)),
+          "roundtrip");
+
+        trck.checkpoint(1);
+    }
+}
+//------------------------------------------------------------------------------
 auto main(int argc, const char** argv) -> int {
-    eagitest::suite test{argc, argv, "vertex attributes", 1};
+    eagitest::suite test{argc, argv, "vertex attributes", 2};
     test.once(vertex_attrib_kinds_all);
+    test.once(vertex_attrib_kinds_index);
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
