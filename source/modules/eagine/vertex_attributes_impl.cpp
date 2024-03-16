c37aa2a797785935887ea85b8e98f45eeb58e3ba
Matus Chochlik <chochlik@gmail.com>
2024-03-16 07:01:33 +0100

    Small changes in vertex attributes

--- source/modules/eagine/vertex_attributes_impl.cpp
+++ source/modules/eagine/vertex_attributes_impl.cpp
@@ -93,14 +93,14 @@ auto default_attrib_value(const vertex_attrib_kind attr) noexcept
         case vertex_attrib_kind::opposite_length:
         case vertex_attrib_kind::face_area:
         case vertex_attrib_kind::weight:
-        case vertex_attrib_kind::scalar_field:
         case vertex_attrib_kind::roughness:
         case vertex_attrib_kind::occlusion:
-        case vertex_attrib_kind::object_id:
         case vertex_attrib_kind::instance_scale:
             return 1.F;
+        case vertex_attrib_kind::scalar_field:
         case vertex_attrib_kind::pointiness:
             return 0.5F;
+        case vertex_attrib_kind::object_id:
         case vertex_attrib_kind::polygon_id:
         case vertex_attrib_kind::material_id:
             return 0;
