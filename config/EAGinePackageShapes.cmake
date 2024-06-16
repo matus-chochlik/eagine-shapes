4c27bd7b9205a5c2403a2feb045063e198c6010d
Matus Chochlik <chochlik@gmail.com>
2024-06-16 11:13:45 +0200

    Added the examples install component + updated sub-modules

--- config/EAGinePackageShapes.cmake
+++ config/EAGinePackageShapes.cmake
@@ -6,9 +6,12 @@
 # Package specific options
 #  Debian
 #   Dependencies
-set(CPACK_DEBIAN_SHAPES-APPS_PACKAGE_DEPENDS "")
+set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
+set(CPACK_DEBIAN_SHAPES-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd-dev,zlib1g-dev")
+set(CPACK_DEBIAN_SHAPES-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd-dev,zlib1g-dev")
 set(CPACK_DEBIAN_SHAPES-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@)")
 #   Descriptions
 set(CPACK_DEBIAN_-SHAPES-APPS_DESCRIPTION "Collection of EAGine geometry shape generator applications.")
+set(CPACK_DEBIAN_-SHAPES-EXAMPLES_DESCRIPTION "EAGine shapes examples.")
 set(CPACK_DEBIAN_-SHAPES-DEV_DESCRIPTION "C++ generator of geometric shapes used for rendering.")
 
