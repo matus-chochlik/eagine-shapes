87d440a62cbb9783cad50268480bb52a60983c7c
Matus Chochlik <chochlik@gmail.com>
2024-06-18 19:55:04 +0200

    Updated sub-modules

--- config/EAGinePackageShapes.cmake
+++ config/EAGinePackageShapes.cmake
@@ -7,8 +7,8 @@
 #  Debian
 #   Dependencies
 set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
-set(CPACK_DEBIAN_SHAPES-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd-dev,zlib1g-dev")
-set(CPACK_DEBIAN_SHAPES-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd-dev,zlib1g-dev")
+set(CPACK_DEBIAN_SHAPES-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
+set(CPACK_DEBIAN_SHAPES-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
 set(CPACK_DEBIAN_SHAPES-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@)")
 #   Descriptions
 set(CPACK_DEBIAN_-SHAPES-APPS_DESCRIPTION "Collection of EAGine geometry shape generator applications.")
